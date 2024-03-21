230cedde00160f0df222236894f81c6103795236
quando <quanchopper1234@gmail.com>
2024-03-21 21:50:56 +0700

    update

--- components/Header.tsx
+++ components/Header.tsx
@@ -24,7 +24,7 @@ const Header = async () => {
             <Link href="/docs" className={'font-bold text-[#081540]'}>
               Insights
             </Link>
-            <Link href="/docs" className={'font-bold text-[#081540]'}>
+            <Link href="/social" className={'font-bold text-[#081540]'}>
               Social Marketplace
             </Link>
             <Link href="/docs" className={'font-bold text-[#081540]'}>
