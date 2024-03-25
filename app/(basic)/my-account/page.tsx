e03981bba6e2e561ebaf892d73c224f28bf605ab
LAPTOP-TED0M108\admin <hoainamnguyen2001@gmail.com>
2024-03-25 12:45:23 +0700

    update form component account

--- app/(basic)/my-account/page.tsx
+++ app/(basic)/my-account/page.tsx
@@ -20,6 +20,7 @@ import { IUserProfile } from "@/type/user-profile.interface";
 import { Metadata } from "next";
 import Image from "next/image";
 import React, { use } from "react";
+import FormSchema from "./FormSchema";
 
 // export const metadata: Metadata = {
 //   title: "My Account",
@@ -27,8 +28,8 @@ import React, { use } from "react";
 // };
 
 const MyAccount = async () => {
-  const userProfile: IUserProfile = await getRequest("/auth/user-profile")
-  console.log(userProfile)
+  // const userProfile: IUserProfile = await getRequest("/auth/user-profile")
+  // console.log(userProfile)
   return (
     <div className="container">
       <PersonalTab key="my-account"></PersonalTab>
@@ -38,112 +39,7 @@ const MyAccount = async () => {
 
         {/* Account Information */}
         <div className="flex flex-col gap-8 w-full">
-          <div className="flex flex-col gap-4">
-            <span className="text-3xl leading-[48px] font-[900]">
-              Account Information
-            </span>
-            <div className="flex gap-4 w-full">
-              <div className="flex flex-col gap-2">
-                <span className="font-bold text-lg ">First Name</span>
-                <Input
-                  type="text"
-                  placeholder="First Name"
-                  className="border-black border"
-                  value={userProfile.first_name}
-                />
-              </div>
-
-              <div className="flex flex-col gap-2 w-full">
-                <span className="font-bold text-lg ">Last Name</span>
-                <Input
-                  type="text"
-                  placeholder="Last Name"
-                  className="border-black border"
-                  value={userProfile.last_name}
-                />
-              </div>
-            </div>
-
-            <div className="flex flex-col gap-2">
-              <span className="font-bold text-lg ">Email Address</span>
-              <Input
-                type="email"
-                placeholder="Email Address"
-                className=" border-black border"
-                value={userProfile.email}
-              />
-            </div>
-
-            <div className="flex flex-col gap-2">
-              <span className="font-bold text-lg ">Phone Number</span>
-              <div className="flex gap-4">
-                <Select>
-                  <SelectTrigger className="border border-black w-40">
-                    <SelectValue placeholder="VN: +84" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectGroup>
-                      <SelectLabel>VN: {userProfile.country.code}</SelectLabel>
-                    </SelectGroup>
-                  </SelectContent>
-                </Select>
-                <Input
-                  type="text"
-                  placeholder="Enter office phone number"
-                  className="border-black border"
-                />
-              </div>
-            </div>
-
-            <div className="flex flex-col gap-2">
-              <span className="font-bold text-lg ">Country of residence</span>
-              <Select>
-                <SelectTrigger className="border border-black">
-                  <SelectValue placeholder={userProfile.country.name} />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectGroup>
-                    <SelectLabel>Việt Nam</SelectLabel>
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-            </div>
-          </div>
-
-          <div className="flex flex-col gap-4">
-            <span className="text-3xl leading-[48px] font-bold ">
-              Password Change
-            </span>
-            <div className="flex flex-col gap-2">
-              <span className="font-bold text-lg ">Old password</span>
-              <Input
-                type="text"
-                placeholder="Enter Old password"
-                className=" border-black border"
-              />
-            </div>
-
-            <div className="flex flex-col gap-2">
-              <span className="font-bold text-lg ">New Password</span>
-              <Input
-                type="text"
-                placeholder="Enter New Password"
-                className=" border-black border"
-              />
-            </div>
-
-            <div className="flex flex-col gap-2">
-              <span className="font-bold text-lg ">Confirm New Password</span>
-              <Input
-                type="text"
-                placeholder="Enter Confirm New Password"
-                className=" border-black border"
-              />
-            </div>
-            <div className="flex justify-end">
-              <Button className="px-7 py-2">Save</Button>
-            </div>
-          </div>
+          <FormSchema />
         </div>
 
         {/* Notification */}
