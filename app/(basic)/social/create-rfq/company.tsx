b2d57f340c3662e2a20c02e05707acaf79cb53a6
Trương Thanh Tùng <88636809+TruongThanhTungDev@users.noreply.github.com>
2024-03-28 22:59:42 +0700

    update create rfq

--- app/(basic)/social/create-rfq/company.tsx
+++ app/(basic)/social/create-rfq/company.tsx
@@ -1,100 +1,226 @@
-'use client'
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
-import { useState } from "react"
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
-import { Textarea } from "@/components/ui/textarea"
+"use client";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { useEffect, useState } from "react";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { Textarea } from "@/components/ui/textarea";
+import { IUserProfile } from "@/type/user-profile.interface";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 
-const CompanyInformation = () => {
-  const [selectedBussiness, setSelectedBussiness] = useState(null)
+const CompanyInformation = ({
+  infoUser,
+  country,
+  company,
+  onSubmit,
+}: {
+  infoUser: IUserProfile;
+  country: any[];
+  company: any;
+  onSubmit: any;
+}) => {
+  console.log("company :>> ", company);
+  const formSchema = z.object({
+    name: z.string().min(1, "Please type Company Name"),
+    type: z.object({
+      code: z.string(),
+    }),
+    location: z.object({
+      code: z.string(),
+      name: z.string(),
+    }),
+    website: z.string(),
+    revenue: z.number(),
+    number_members: z.number(),
+    address: z.string(),
+    description: z.string(),
+  });
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: company.name ? company.name : "",
+      type: {
+        code: company.type.code ? company.type.code : "",
+      },
+      location: {
+        code: company.location ? company.location.code : "",
+        name: company.location ? company.location.name : "",
+      },
+      website: company.website ? company.website : "",
+      revenue: company.revenue ? company.revenue : 0,
+      address: company.address ? company.address : "",
+      description: company.description ? company.description : "",
+      number_members: company.number_members ? company.number_member : 0,
+    },
+  });
+  const handleSubmit = (values: z.infer<typeof formSchema>) => {
+    onSubmit(values);
+  };
   return (
     <Accordion type="single" collapsible>
-      <AccordionItem value="item-1" className='border-b-0'>
-        <AccordionTrigger className='border-primary text-2xl font-[600] !py-0'>Company Information</AccordionTrigger>
-        <AccordionContent className='pt-2 flex flex-col gap-4'>
+      <AccordionItem value="item-1" className="border-b-0">
+        <AccordionTrigger className="border-primary text-2xl font-[600] !py-0">
+          Company Information
+        </AccordionTrigger>
+        <AccordionContent className="pt-2 flex flex-col gap-4">
           <div>
-            Editing these will also update your company information in your settings
+            Editing these will also update your company information in your
+            settings
           </div>
-          <div>
-            <Label className="text-base">Company Name *</Label>
-            <Input placeholder="Your Name" />
-          </div>
-          <div>
-            <Label className="text-base">Company Name *</Label>
-            <Select>
-              <SelectTrigger>
-                <SelectValue placeholder="-Select Bussiness Type-" />
-              </SelectTrigger>
-              <SelectContent>
-              </SelectContent>
-            </Select>
+          <Form {...form} handleSubmit={() => form.handleSubmit(handleSubmit)}>
             <div>
-              {selectedBussiness ? selectedBussiness : ''}
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => {
+                  return (
+                    <FormItem>
+                      <FormLabel className="text-base text-neutral-400">
+                        Company Name *
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Company Name"
+                          type="text"
+                          {...field}
+                          className="border-black border"
+                          disabled
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  );
+                }}
+              ></FormField>
+              {/* <Label className="c">Company Name *</Label>
+              <Input placeholder="Your Name" value={company.name} /> */}
             </div>
-          </div>
-          <div>
-            <Label className="text-base">Company Country *</Label>
-            <Select>
-              <SelectTrigger>
-                <SelectValue placeholder="Vietnam" />
-              </SelectTrigger>
-              <SelectContent>
-              </SelectContent>
-            </Select>
-          </div>
-          <div>
-            <Label className="text-base">Year Established</Label>
-            <Select>
-              <SelectTrigger>
-                <SelectValue placeholder="-Select Year-" />
-              </SelectTrigger>
-              <SelectContent>
-              </SelectContent>
-            </Select>
-          </div>
-          <div>
-            <Label className="text-base">Number of Employees</Label>
-            <Select>
-              <SelectTrigger>
-                <SelectValue placeholder="-Select Number of Employees-" />
-              </SelectTrigger>
-              <SelectContent>
-              </SelectContent>
-            </Select>
-          </div>
-          <div>
-            <Label className="text-base">Annual Sales Revenue</Label>
-            <Select>
-              <SelectTrigger>
-                <SelectValue placeholder="-Select Annual Sales Revenue-" />
-              </SelectTrigger>
-              <SelectContent>
-              </SelectContent>
-            </Select>
-          </div>
-          <div>
-            <Label className="text-base">Company Website</Label>
-            <Input placeholder="Enter company website URL" className="mb-[8px]" />
-            <RadioGroup>
-              <div className="flex items-center space-x-2">
-                <RadioGroupItem value="default" id="r1" />
-                <Label htmlFor="r1">My company has no website.</Label>
-              </div>
-            </RadioGroup>
-          </div>
-          <div>
-            <Label className="text-base">Company Address</Label>
-            <Input placeholder="Enter company address" />
-          </div>
-          <div>
-            <Label className="text-base">Company Description</Label>
-            <Textarea className="border-black border-[1px]" placeholder="Enter company description" />
-          </div>
+            <div>
+              <Label className="text-base">Business Type</Label>
+              <Select>
+                <SelectTrigger>
+                  <SelectValue placeholder="-Select Bussiness Type-" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectItem value="farming_production_processing_packing">
+                      Farming / Production / Processing / Packing
+                    </SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+              <div>{company.type.name}</div>
+            </div>
+            <div>
+              <Label className="text-base">Company Country *</Label>
+              <Select defaultValue={company.location.code}>
+                <SelectTrigger>
+                  <SelectValue
+                    placeholder="Company Country"
+                    defaultValue={company.location.code}
+                  />
+                </SelectTrigger>
+                <SelectContent>
+                  {country.length &&
+                    country.map((item: any) => (
+                      <SelectItem key={item.code} value={item.code}>
+                        {item.name}
+                      </SelectItem>
+                    ))}
+                </SelectContent>
+              </Select>
+            </div>
+            <div>
+              <Label className="text-base">Year Established</Label>
+              <Select>
+                <SelectTrigger>
+                  <SelectValue placeholder="-Select Year-" />
+                </SelectTrigger>
+                <SelectContent></SelectContent>
+              </Select>
+            </div>
+            <div>
+              <Label className="text-base">Number of Employees</Label>
+              <Select defaultValue={company.number_members.toString()}>
+                <SelectTrigger>
+                  <SelectValue
+                    placeholder="-Select Number of Employees-"
+                    defaultValue={company.number_members.toString()}
+                  />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="101">101</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+            <div>
+              <Label className="text-base">Annual Sales Revenue</Label>
+              <Select defaultValue={company.revenue.toString()}>
+                <SelectTrigger>
+                  <SelectValue placeholder="-Select Annual Sales Revenue-" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="21">21</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+            <div>
+              <Label className="text-base">Company Website</Label>
+              <Input
+                placeholder="Enter company website URL"
+                className="mb-[8px]"
+                value={infoUser.company.website}
+              />
+              <RadioGroup>
+                <div className="flex items-center space-x-2">
+                  <RadioGroupItem value="" id="r1" />
+                  <Label htmlFor="r1">My company has no website.</Label>
+                </div>
+              </RadioGroup>
+            </div>
+            <div>
+              <Label className="text-base">Company Address</Label>
+              <Input
+                placeholder="Enter company address"
+                value={infoUser.company.address}
+              />
+            </div>
+            <div>
+              <Label className="text-base">Company Description</Label>
+              <Textarea
+                className="border-black border-[1px]"
+                placeholder="Enter company description"
+                value={infoUser.company.description}
+              />
+            </div>
+          </Form>
         </AccordionContent>
       </AccordionItem>
     </Accordion>
-  )
-}
-export default CompanyInformation
+  );
+};
+export default CompanyInformation;
--- app/(basic)/social/create-rfq/page.tsx
+++ app/(basic)/social/create-rfq/page.tsx
@@ -1,14 +1,38 @@
+"use client"
 import ContactInformation from "./contact"
 import CompanyInformation from "./company"
 import { Metadata } from "next"
 import Request from "./request";
+import { getServerSession } from "next-auth";
+import { options } from "@/app/api/auth/[...nextauth]/options";
+import { IUserProfile } from "@/type/user-profile.interface";
+import { getRequest } from "@/hook/api";
 
-export const metadata: Metadata = {
-  title: "Create RFQ",
-  description: "Create RFQ",
-};
+// export const metadata: Metadata = {
+//   title: "Create RFQ",
+//   description: "Create RFQ",
+// };
 
-const Create = () => {
+const Create = async () => {
+  const session = await getServerSession(options);
+  const user: IUserProfile = session?.user;
+  const [userData, countryData, companyData] = await Promise.all([
+    user ? getRequest("/user/profile") : null,
+    user ? getRequest("/config/countries") : null,
+    user ? getRequest("/user/company") : null,
+  ]);
+  const userProfile: IUserProfile = {
+    ...userData.data,
+    country: {
+      ...userData.data.country,
+      code: "+" + userData.data.country.code
+    },
+  };
+  const countries: any[] = countryData.data;
+  const company: any = companyData.data
+  const updateCompany = (value: any) => {
+    
+  }
   return (
     <div className="w-1/3 mx-auto py-8 flex flex-col gap-10">
       <div className="flex flex-col gap-4">
@@ -27,20 +51,26 @@ const Create = () => {
         </div>
       </div>
       <div className="flex flex-col gap-4">
-        <div className="text-3xl font-bold">
-          My Information
-        </div>
+        <div className="text-3xl font-bold">My Information</div>
         <div className="text-base">
           You must fill out this section to insert your request details.
         </div>
       </div>
       <div>
-        <ContactInformation />
+        <ContactInformation info={userProfile} country={countries} />
       </div>
       <div>
-        <CompanyInformation />
+        <CompanyInformation
+          infoUser={userProfile}
+          country={countries}
+          company={company}
+          onSubmit={updateCompany}
+        />
       </div>
-      <button className="h-[42px] bg-primary text-white font-bold leading-[20px] w-full rounded-[6px]">
+      <button
+        className="h-[42px] bg-primary text-white font-bold leading-[20px] w-full rounded-[6px]"
+        onClick={() => updateCompany}
+      >
         Save
       </button>
       <div>
