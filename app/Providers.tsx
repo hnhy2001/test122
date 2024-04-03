2fd26f03993a1844c87e56832aa14ce465d3d018
quando <quanchopper1234@gmail.com>
2024-04-03 11:21:38 +0700

    fix http

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -1,12 +1,22 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
-import React, { ReactNode } from "react";
+import React, { ReactNode, useEffect } from "react";
 
 interface Props {
   children: ReactNode;
 }
 const Providers = ({ children }: Props) => {
+  useEffect(() => {
+    const metaTag = document.createElement('meta');
+    metaTag.httpEquiv = 'Content-Security-Policy';
+    metaTag.content = 'upgrade-insecure-requests';
+  
+    if (location.protocol === 'https:') {
+      document.head.appendChild(metaTag);
+    }
+  
+  }, []);
   return <SessionProvider>{children}</SessionProvider>;
 };
 
