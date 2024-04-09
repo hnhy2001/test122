b252eb8e2c9afeb76f9b29275a0801624ccd6412
quando <quanchopper1234@gmail.com>
2024-04-09 17:37:05 +0700

    fix

--- components/auth/SignOut.tsx
+++ components/auth/SignOut.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 const SignOut = () => {
     return (
         <div onClick={() => {
-            signOut({ callbackUrl: window.location.href });
+            signOut();
         }}>Sign Out</div>
     )
 }
