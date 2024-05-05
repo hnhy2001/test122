4ea47fe20202ff1309a75613ed5eb8ffd40b2603
LAPTOP-TED0M108\admin <hoainamnguyen2001@gmail.com>
2024-05-05 12:20:40 +0700

    fix

--- app/(auth)/confirm-email/page.tsx
+++ app/(auth)/confirm-email/page.tsx
@@ -1,24 +1,39 @@
-﻿import { getRequest } from "@/hook/api";
-import Link from "next/link";
+﻿import { Button } from "@/components/ui/button";
+import { getRequest } from "@/hook/api";
+import Image from "next/image";
 import React from "react";
 
 const ConfirmEmail = async (props: any) => {
   const token = props.searchParams?.token || "";
+  let check = -1;
   const [data] = await Promise.all([
-    getRequest("/auth/verify-account/" + token),
+    getRequest("/auth/verify-account/" + token)
+      .then((res: any) => {
+        check = 1;
+      })
+      .catch((err) => (check = -1)),
   ]);
-  const content =
-    data.is_success == 1
-      ? "Thank you for confirming the email successfully. You can login now"
-      : "Verifing the email failed";
 
   return (
     <div className="text-center pt-8">
-      {data.is_success == 1
-        ? <div className="text-3xl font-bold">
-          <span>Thank you for confirming the email successfully. </span> <Link className="underline" href={"/signin"}>You can login now</Link>
+      {check == -1 ? (
+        <div className="text-3xl font-bold flex flex-col justify-center">
+          <Image
+            src="/verify-success.png"
+            width={64}
+            height={64}
+            alt="delete"
+            className="w-6 h-6 cursor-pointer"
+          />
+          <span>Thank you for confirming the email successfully. </span>{" "}
+          {/* <Link className="underline" href={"/signin"}>
+            <Button>You can login now</Button>
+          </Link> */}
+          <Button>ok</Button>
         </div>
-        : "Verifing the email failed"}
+      ) : (
+        "Verifing the email failed"
+      )}
     </div>
   );
 };
